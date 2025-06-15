6256fb910f7b60fdc8daff7d446aacae1f15afbf
Your Name <you@example.com>
2025-06-15 21:50:28 +0800

    5

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dashboard">
+    <h2>仪表盘</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 20px;
+}
+</style> 
